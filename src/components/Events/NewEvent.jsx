1f5ea6e63dfd2021974799d9cfa6b0294ba75e39
MehmetKaldirim <mathdenizi@gmail.com>
2024-03-05 01:21:33 +0100

    new event added with mutation query

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -5,15 +5,21 @@ import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
 import { createNewEvent } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
+import { queryClient } from "../../util/http.js";
 
 export default function NewEvent() {
   const navigate = useNavigate();
   const { mutate, isPending, isError, error } = useMutation({
     mutationFn: createNewEvent,
+    onSuccess:()=>{
+      queryClient.invalidateQueries({queryKey:['events']});//after success mutation i ll fetch all query again with this queryKey
+      navigate('/events');
+    }
   });
 
   function handleSubmit(formData) {
     mutate({ event: formData });
+    
   }
 
   return (
